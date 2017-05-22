d9c50a0d59d05afffe4176786be40faf54119cb4
Alexander Tupavov <tauinbox@gmail.com>
2017-05-22 18:59:58 +0300

    test data extended

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,10 +1,10 @@
 import {Component} from '@angular/core';
 import { FormioAuthService } from 'ng2-formio/auth';
-import { FORM } from './data';
+import { FORM2 } from './data';
 @Component({
   template: require('./home.html')
 })
 export class HomeComponent {
-  private formdata: any = FORM;
+  private formdata: any = FORM2;
   constructor(public auth: FormioAuthService) {}
 }
