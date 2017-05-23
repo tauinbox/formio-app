a6652e5be8fdd9ec65c7503cbf68c580f1e684d2
Alexander Tupavov <tauinbox@gmail.com>
2017-05-23 12:41:07 +0300

    added form data

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,10 +1,10 @@
 import {Component} from '@angular/core';
 import { FormioAuthService } from 'ng2-formio/auth';
-import { FORM2 } from './data';
+import { FORM } from './data';
 @Component({
   template: require('./home.html')
 })
 export class HomeComponent {
-  private formdata: any = FORM2;
+  private formdata: any = FORM;
   constructor(public auth: FormioAuthService) {}
 }
