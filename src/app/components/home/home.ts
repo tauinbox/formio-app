c8e9e39a87061539d304be442822da75493b17f7
Alexander Tupavov <tauinbox@gmail.com>
2017-05-24 11:59:09 +0300

    clean up some mess

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,10 +1,12 @@
 import {Component} from '@angular/core';
-import { FormioAuthService } from 'ng2-formio/auth';
+// import { FormioAuthService } from 'ng2-formio/auth';
 import { FORM } from './data';
+
 @Component({
   template: require('./home.html')
 })
+
 export class HomeComponent {
   private formdata: any = FORM;
-  constructor(public auth: FormioAuthService) {}
+  // constructor(public auth: FormioAuthService) {}
 }
